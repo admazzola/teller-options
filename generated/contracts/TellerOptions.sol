316bd72ca4169109ae0876d2503cd3cec50ee62c
Admazzola <admazzola@gmail.com>
2021-08-21 14:58:27 -0400

    adding 721 recieve

--- generated/contracts/TellerOptions.sol
+++ generated/contracts/TellerOptions.sol
@@ -181,19 +181,21 @@ interface IERC20 {
 
  
  
- 
-   
-
-interface MintableERC20  {
-     function mint(address account, uint256 amount) external ;
-     function burn(address account, uint256 amount) external ;
+ interface ERC721TokenReceiver {
+    /// @notice Handle the receipt of an NFT
+    /// @dev The ERC721 smart contract calls this function on the recipient
+    ///  after a `transfer`. This function MAY throw to revert and reject the
+    ///  transfer. Return of other than the magic value MUST result in the
+    ///  transaction being reverted.
+    ///  Note: the contract address is always the message sender.
+    /// @param _operator The address which called `safeTransferFrom` function
+    /// @param _from The address which previously owned the token
+    /// @param _tokenId The NFT identifier which is being transferred
+    /// @param _data Additional data with no specified format
+    /// @return `bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"))`
+    ///  unless throwing
+    function onERC721Received(address _operator, address _from, uint256 _tokenId, bytes _data) external returns(bytes4);
 }
-
- 
- 
-abstract contract ApproveAndCallFallBack {
-       function receiveApproval(address from, uint256 tokens, address token, bytes memory data) public virtual;
-  }
   
   
   /*
@@ -209,7 +211,7 @@ abstract contract ApproveAndCallFallBack {
  *  Staking contract that supports community-extractable donations 
  *
  */
-contract TellerOptions  {
+contract TellerOptions is ERC721TokenReceiver {
  
   
  /* address public _stakeableCurrency; 
@@ -316,7 +318,20 @@ contract TellerOptions  {
   
   }
    
-    
+   function onERC721Received(
+        address operator,
+        address from,
+        uint256 id,
+        bytes calldata data
+    ) external returns (bytes4) {
+
+        return
+            bytes4(
+                keccak256(
+                    "onERC721Received(address,address,uint256,bytes)"
+                )
+            );
+    }
    
      // ------------------------------------------------------------------------
 
